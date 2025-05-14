32d18327b7533b4f5d71363b35c301cf7976abc6
AlinaDragu <alina_elena14@yahoo.com>
2025-05-14 19:36:49 +0300

    PY4E

--- PY4E/8.Lists/ex_08_04.py
+++ PY4E/8.Lists/ex_08_04.py
@@ -1,7 +1,7 @@
 # Open the file romeo.txt and read it line by line.
 # For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. 
 # When the program completes, sort and print the resulting words in python sort() order as shown in the desired output.
-
+#Run with terminal
 
 fname = input("Enter file name: ")
 fh = open(fname)
