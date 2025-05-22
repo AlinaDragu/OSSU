fa969e981973da0bda0d108c31f8d6709045fb71
AlinaDragu <alina_elena14@yahoo.com>
2025-05-22 20:10:02 +0300

    Intro CS

---
+++ Intro CS/Lecture3/finger exercise.py
@@ -0,0 +1,12 @@
+# Assume you are given a positive integer variable named N. Write a piece of Python code that prints hello world on separate lines, N times. 
+# You can use either a while loop or a for loop.
+
+
+
+N = 5
+L = 0
+
+while L < N :
+    print('Hello')
+    print('World')
+    L += 1
