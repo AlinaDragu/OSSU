bbe7f97b0db34c562b307f7ac9e582e5e38925aa
AlinaDragu <alina_elena14@yahoo.com>
2025-05-23 19:03:36 +0300

    Intro CS

---
+++ Intro CS/Lecture3/mysum.py
@@ -0,0 +1,10 @@
+#fix this code to use variables "start" and "end" in the "range" , to get the total sum between and including those valiues.
+#for example , if "start = 3" and "end = 5" then the sum should be 12.
+
+mysum = 0
+start = 3
+end = 5
+for i in range (start,end+1):
+    print('i=',i)
+    mysum += i
+print(mysum)
