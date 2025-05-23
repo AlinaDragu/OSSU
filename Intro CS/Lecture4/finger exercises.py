9cd5692b1fba9dd43e7de491c7f295c0b62c6b11
AlinaDragu <alina_elena14@yahoo.com>
2025-05-23 18:04:05 +0300

    Intro CS

--- Intro CS/Lecture4/finger exercises.py
+++ Intro CS/Lecture4/finger exercises.py
@@ -3,3 +3,13 @@
 # The code prints the cube root if N is a perfect cube or it prints error if N is not a perfect cube. 
 # Hint: use a loop that increments a counter—you decide when the counter should stop.
 
+N = int(input('Enter a positive integer: '))
+i = 0
+
+while i ** 3 <= N:
+    if i ** 3 == N:
+        print("Cube root:", i)
+        break
+    i += 1
+else:
+    print('its an error')
