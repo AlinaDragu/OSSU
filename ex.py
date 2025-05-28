b1e054a2f652c87027c2db5114d034368957dfa5
AlinaDragu <alina_elena14@yahoo.com>
2025-05-28 19:24:10 +0300

    Intro CS

--- ex.py
+++ ex.py
@@ -1,36 +1,71 @@
-# FOR loop challenges
+# # FOR loop challenges
 
-# 1. print numbers 1 to 10
+# #1. print numbers 1 to 10
+# # Expected Output:
+# # 1
+# # 2
+# # ...
+# # 10
 
-i = 0
+# i = 0
 
-for i in range(1,11):
-    print(i)
+# for i in range(1,11):
+#     print(i)
 
 
 
-#2. print even numbers from 2 to 20
+# #2. print even numbers from 2 to 20
+# # Expected Output:
+# # 2
+# # 4
+# # ...
+# # 20
 
-i = 0
+# i = 0
 
-for i in range(2,21):
-    if i % 2 == 0 :
-        print(i)
+# for i in range(2,21):
+#     if i % 2 == 0 :
+#         print(i)
         
         
 
-#3. print each letter in a word
+# #3. print each letter in a word
+# # Input: apple
+# # Output:
+# # a
+# # p
+# # p
+# # l
+# # e
 
-word = input("Type your word here: ")
+# word = input("Type your word here: ")
 
-for w in word :
-    print(w)
+# for w in word :
+#     print(w)
     
     
     
-#4. loop through a list of items
+# #4. loop through a list of items
+# # Print: "Item: book", etc.
 
-items = ["book", "pen", "laptop", "phone"]
+# items = ["book", "pen", "laptop", "phone"]
 
-for item in items:
-    print('Item:',item)
+# for item in items:
+#     print('Item:',item)
+    
+    
+    
+
+
+# WHILE loop challenges
+
+
+#5. count until 5
+# Use a while loop to count from 1 to 5 and print each number
+
+y = 1
+
+while y <= 5:
+    print(y)
+    y += 1
+    
