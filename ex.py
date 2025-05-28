54b7dcb8a0ec12869b1e3094be2dd12c8dd82e66
AlinaDragu <alina_elena14@yahoo.com>
2025-05-28 18:56:18 +0300

    Intro CS

--- ex.py
+++ ex.py
@@ -1,11 +1,18 @@
 #FOR loop challenges
 
-#1. print numbers 1 to 10
+# 1. print numbers 1 to 10
 
 i = 0
 
-for i in range (1,11):
+for i in range(1,11):
     print(i)
 
 
 
+#2. print even numbers from 2 to 20
+
+i = 0
+
+for i in range(2,21):
+    if i % 2 == 0 :
+        print(i)
