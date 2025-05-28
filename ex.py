f8c58893d1297f3d72350513a4e551da2933812c
AlinaDragu <alina_elena14@yahoo.com>
2025-05-28 19:13:57 +0300

    Intro CS

--- ex.py
+++ ex.py
@@ -1,30 +1,36 @@
 #FOR loop challenges
 
-# 1. print numbers 1 to 10
+# # 1. print numbers 1 to 10
 
-i = 0
+# i = 0
 
-for i in range(1,11):
-    print(i)
+# for i in range(1,11):
+#     print(i)
 
 
 
-#2. print even numbers from 2 to 20
+# #2. print even numbers from 2 to 20
 
-i = 0
+# i = 0
 
-for i in range(2,21):
-    if i % 2 == 0 :
-        print(i)
+# for i in range(2,21):
+#     if i % 2 == 0 :
+#         print(i)
         
         
 
-#3. print each letter in a word
+# #3. print each letter in a word
 
-word = input("Type your word here: ")
+# word = input("Type your word here: ")
 
-for w in word :
-    print(w)
+# for w in word :
+#     print(w)
     
     
     
+#4. loop through a list of items
+
+items = ["book", "pen", "laptop", "phone"]
+
+for item in items:
+    print('Item:',item)
