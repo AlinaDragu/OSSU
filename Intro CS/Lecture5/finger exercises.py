d2f6b09f3379473d8bcb8d12252f5c3381d3e1f8
AlinaDragu <alina_elena14@yahoo.com>
2025-06-03 12:23:09 +0300

    Intro CS

--- Intro CS/Lecture5/finger exercises.py
+++ Intro CS/Lecture5/finger exercises.py
@@ -2,3 +2,10 @@
 # Write a piece of Python code that prints out a new string containing the even indexed characters of my_str.
 # For example, if my_str = "abcdefg" then your code should print out aceg.
 
+my_str = 'abcdefg'
+s = ''
+
+for i in range(0,len(my_str),2):
+    s += my_str[i]
+
+print(s)
