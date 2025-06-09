357a85b5d0484a8e8343286dd73c37b0cea36b08
AlinaDragu <alina_elena14@yahoo.com>
2025-06-09 16:52:55 +0300

    Intro CS

--- Intro CS/Lecture6/finger exercises.py
+++ Intro CS/Lecture6/finger exercises.py
@@ -3,7 +3,18 @@
 # The code prints two lines: count: with how many guesses it took to find N, and answer: with the value of N.
 # Hints: If the halfway value is exactly in between two integers, choose the smaller one.
 
-N = int(input("Enter a number between 0 and 1000: "))  # or set N manually
+N = int(input("Enter a number between 0 and 1000: ")) 
 low = 0
 high = 1000
-count = 0
+
+
+for count in range(1,21):
+    guess = (low + high) // 2
+    if guess == N:
+        print(f"count: {count}")
+        print(f"answer: {guess}")
+        break
+    elif guess < N:
+        low = guess + 1
+    else:
+        high = guess - 1
