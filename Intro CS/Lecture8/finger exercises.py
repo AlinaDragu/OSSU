6ce4f86809b2a3045d66a531230f2944e67c6cc4
AlinaDragu <alina_elena14@yahoo.com>
2025-06-22 19:26:51 +0300

    Intro CS

--- Intro CS/Lecture8/finger exercises.py
+++ Intro CS/Lecture8/finger exercises.py
@@ -1,11 +1,11 @@
 Implement the function that meets the specifications below:
 
 def same_chars(s1, s2):
-    """
-    s1 and s2 are strings
-    Returns boolean True is a character in s1 is also in s2, and vice 
-    versa. If a character only exists in one of s1 or s2, returns False.
-    """
+    # """
+    # s1 and s2 are strings
+    # Returns boolean True is a character in s1 is also in s2, and vice 
+    # versa. If a character only exists in one of s1 or s2, returns False.
+    # """
     # Your code here
 
 # Examples:
