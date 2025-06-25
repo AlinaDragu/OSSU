77a3f131d7a94e253175358d78e7c9e324271a8a
AlinaDragu <alina_elena14@yahoo.com>
2025-06-25 19:33:00 +0300

    Intro CS

--- Intro CS/Lecture6/cube root.py
+++ Intro CS/Lecture6/cube root.py
@@ -5,3 +5,14 @@ cube = 27
 epsilon = 0.01
 low = 0
 high = cube
+
+guess = (high+low)/2
+while abs(guess**3-cube) > epsilon:
+    if guess**3 > cube :
+        #guess too high
+        high = guess
+    else:
+        low = guess
+    guess = (high+low)/2
+    
+print(guess)
