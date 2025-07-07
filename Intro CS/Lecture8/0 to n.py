5e1218881a5c5ca8eae728e63c9d637bfadb3465
AlinaDragu <alina_elena14@yahoo.com>
2025-07-07 17:00:30 +0300

    Intro CS

--- Intro CS/Lecture8/0 to n.py
+++ Intro CS/Lecture8/0 to n.py
@@ -5,10 +5,19 @@ def apply(criteria,n):
     (i.e. return True when criteria is applied to them)
     """ 
     # your code here
+    count = 0
+    for i in range(n+1):
+        if criteria(i): #is a boolean
+            count += 1
+    return count
 
 
 def is_even(x):
     return x%2==0
 
+def is_five(x):
+    return x == 5
+
 how_many = apply(is_even,10)
-# print(how_many)
+how_manyy = apply(is_five,10)
+print(how_many,how_manyy)
