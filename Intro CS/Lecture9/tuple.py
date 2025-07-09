57eafecb84fa1950c238de622a7a10cc6253da3c
AlinaDragu <alina_elena14@yahoo.com>
2025-07-09 12:13:36 +0300

    Intro CS

---
+++ Intro CS/Lecture9/tuple.py
@@ -0,0 +1,12 @@
+# Write a function that meets these specifications:
+def char_counts(s):
+    """ s is a string of lowercase chars 
+    Returns a tuple where the first value is the 
+    number of vowels in s and the second value 
+    is the number of consonants in s 
+    """
+    # your code here
+
+
+# print(char_counts("abcd"))  # prints (1,3)
+# print(char_counts("zcght"))  # prints (0,5)
