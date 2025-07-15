4b6873c32e5e0d2b5ae22966f9ccb340fe7c4e69
AlinaDragu <alina_elena14@yahoo.com>
2025-07-15 16:50:26 +0300

    Intro CS

--- Intro CS/Lecture10/finger exercises.py
+++ Intro CS/Lecture10/finger exercises.py
@@ -5,6 +5,16 @@ def all_true(n, Lf):
     with n as a parameter. Otherwise returns False. 
     """
     # Your code here
-
+    flag = True
+    for f in Lf:
+        if not f(n):
+            flag = False
+            break
+    return flag
 # Examples:    
-all_true() # prints 6
+# all_true() # prints 6
+is_even = lambda x: x % 2 == 0
+greater_than_3 = lambda x: x > 3
+
+print(all_true(6, [is_even, greater_than_3]))  # True
+print(all_true(2, [is_even, greater_than_3]))  # False
