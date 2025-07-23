e93a2e744d0ae08414e6db2b52f5056fe697e429
AlinaDragu <alina_elena14@yahoo.com>
2025-07-23 20:10:34 +0300

    Intro CS

--- Intro CS/Lecture10/remove list.py
+++ Intro CS/Lecture10/remove list.py
@@ -6,12 +6,13 @@ def remove_elem(L, e):
     but without any elements equal to e. 
     """
     # your code here
-    newlist = []
-    for i in L:
-        # i is 1 then 2 then 2 then 2
-        if i != e:
-            newlist.append(i)
-    return newlist
+    
+    # newlist = []
+    # for i in L:
+    #     # i is 1 then 2 then 2 then 2
+    #     if i != e:
+    #         newlist.append(i)
+    # return newlist
   
 L = [1,2,2,2]
 print(remove_elem(L, 2))    # prints [1]
