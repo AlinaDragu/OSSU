64dce481a7bc719c3788853c2518d1150c167a9b
AlinaDragu <alina_elena14@yahoo.com>
2025-07-23 20:10:44 +0300

    Intro CS

--- Intro CS/Lecture10/remove list.py
+++ Intro CS/Lecture10/remove list.py
@@ -7,12 +7,12 @@ def remove_elem(L, e):
     """
     # your code here
     
-    # newlist = []
-    # for i in L:
-    #     # i is 1 then 2 then 2 then 2
-    #     if i != e:
-    #         newlist.append(i)
-    # return newlist
+    newlist = []
+    for i in L:
+        # i is 1 then 2 then 2 then 2
+        if i != e:
+            newlist.append(i)
+    return newlist
   
 L = [1,2,2,2]
 print(remove_elem(L, 2))    # prints [1]
