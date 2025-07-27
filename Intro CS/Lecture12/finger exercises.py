490ed9c52a6865ff8faf0231ad2f004b3f69a70c
AlinaDragu <alina_elena14@yahoo.com>
2025-07-27 14:54:38 +0300

    Intro CS

--- Intro CS/Lecture12/finger exercises.py
+++ Intro CS/Lecture12/finger exercises.py
@@ -7,12 +7,12 @@ def count_sqrts(nums_list):
     Returns how many elements in nums_list are exact squares of elements in the same list, including itself.
     """
     # Your code here
-    nums_set = set(nums_list)  # convert the list to a set for faster lookups
-    count = 0  #counter
+    nums_set = set(nums_list) # convert the list to a set for faster lookups
+    count = 0 # counter
     for num in nums_list:
-        root = math.isqrt(num)  # get the integer square root of the number
-        if root * root == num and root in nums_set:   #check if the number is a perfect square and its root is in the list
-            count += 1   # increment
+        root = math.isqrt(num) # get the integer square root of the number
+        if root * root == num and root in nums_set: # check if the number is a perfect square and its root is in the list
+            count += 1  # increment
     return count
 
 # Examples:    
