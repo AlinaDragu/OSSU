d380b35f477475c085869209fb4f747c227fa7c4
AlinaDragu <alina_elena14@yahoo.com>
2025-07-31 12:24:36 +0300

    Intro CS

---
+++ Intro CS/Lecture10/sort.py
@@ -0,0 +1,13 @@
+# Write a function that meets this specification
+def sort_words(sen):
+    """ sen is a string representing a sentence 
+    Returns a list containing all the words in sen but
+    sorted in alphabetical order. """
+    # your code here
+
+
+# s = "look at this photograph"
+# print(sort_words(s))    # prints ['at', 'look', 'photograph', 'this']
+
+# s = "now this is a story all about how my life got flipped turned upside down"
+# print(sort_words(s))
