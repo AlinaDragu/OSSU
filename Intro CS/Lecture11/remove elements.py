5cbfdb2187fa595d37542ec442564087f2fd2822
AlinaDragu <alina_elena14@yahoo.com>
2025-07-31 11:28:59 +0300

    Intro CS

---
+++ Intro CS/Lecture11/remove elements.py
@@ -0,0 +1,19 @@
+## remove from a list
+L = [2,1,3,6,3,7,0]
+L.remove(2)
+L.remove(3)
+del(L[1])
+print(L.pop())
+
+
+#############
+## Removing elements
+#############
+
+L = [2,1,3,6,3,7,0] 
+L.pop(5)
+print(L)
+L.remove(3)
+print(L)
+L.pop()
+print(L)
