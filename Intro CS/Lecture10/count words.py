bd3a39135eec8b4a594eb8a23409ffa5f148e8a2
AlinaDragu <alina_elena14@yahoo.com>
2025-07-31 12:16:20 +0300

    Intro CS

--- Intro CS/Lecture10/count words.py
+++ Intro CS/Lecture10/count words.py
@@ -4,10 +4,12 @@ def count_words(sen):
     Returns how many words are in sen (i.e. a word is a 
     a sequence of characters between spaces. """
     # your code here
+    L1 = sen.split(' ')
+    return len(L1)
+        
 
+s = "Hello it's me"
+print(count_words(s))   # prints 3
 
-# s = "Hello it's me"
-# print(count_words(s))   # prints 3
-
-# s = "I just took a DNA test turns out I'm 100% splitting strings"
-# print(count_words(s))   # prints 12
+s = "I just took a DNA test turns out I'm 100% splitting strings"
+print(count_words(s))   # prints 12
