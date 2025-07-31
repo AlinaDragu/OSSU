3dd6d78f975bdf82ce6956ec1c92ae55e5707300
AlinaDragu <alina_elena14@yahoo.com>
2025-07-31 12:04:27 +0300

    Intro CS

---
+++ Intro CS/Lecture10/count words.py
@@ -0,0 +1,13 @@
+# Write a function that meets this specification
+def count_words(sen):
+    """ sen is a string representing a sentence 
+    Returns how many words are in sen (i.e. a word is a 
+    a sequence of characters between spaces. """
+    # your code here
+
+
+# s = "Hello it's me"
+# print(count_words(s))   # prints 3
+
+# s = "I just took a DNA test turns out I'm 100% splitting strings"
+# print(count_words(s))   # prints 12
