f513bb6e4551957d60899f5c46af33e230fed897
AlinaDragu <alina_elena14@yahoo.com>
2025-08-03 20:31:25 +0300

    Intro CS

--- Intro CS/Lecture12/finger exercises.py
+++ Intro CS/Lecture12/finger exercises.py
@@ -7,6 +7,7 @@ def count_sqrts(nums_list):
     Returns how many elements in nums_list are exact squares of elements in the same list, including itself.
     """
     # Your code here
+    
     nums_set = set(nums_list) # convert the list to a set for faster lookups
     count = 0 # counter
     for num in nums_list:
