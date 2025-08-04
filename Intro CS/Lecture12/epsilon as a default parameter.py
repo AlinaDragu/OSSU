1473bdab944a4a1b5cbc72ecf6cb1a71dc6ce75f
AlinaDragu <alina_elena14@yahoo.com>
2025-08-04 11:02:33 +0300

    Intro CS

--- Intro CS/Lecture12/epsilon as a default parameter.py
+++ Intro CS/Lecture12/epsilon as a default parameter.py
@@ -1,3 +1,18 @@
 ### EXAMPLE: improved bisection square root as a function
 # takes in x and an epsilon as a default parameter
 #########################
+
+def bisection_root_new(x, epsilon=0.01):
+    num_guesses = 0
+    low = 0
+    high = x
+    guess = (high + low)/2.0
+    while abs(guess**2 - x) >= epsilon:
+        if guess**2 < x: 
+            low = guess
+        else: 
+            high = guess
+        guess = (high + low)/2.0
+        num_guesses += 1
+    print('num_guesses =', num_guesses)
+    return guess
