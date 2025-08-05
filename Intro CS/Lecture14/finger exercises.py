134f1ace63fc0a7facc651b0d2d0baa2cdfeb833
AlinaDragu <alina_elena14@yahoo.com>
2025-08-05 11:02:48 +0300

    Intro CS

--- Intro CS/Lecture14/finger exercises.py
+++ Intro CS/Lecture14/finger exercises.py
@@ -12,3 +12,19 @@ def keys_with_value(aDict, target):
 aDict = {1:2, 2:4, 5:2}
 target = 2   
 print(keys_with_value(aDict, target)) # prints the list [1,5]
+
+
+
+
+def all_positive(d):
+    """
+    d is a dictionary that maps int:list
+    Suppose an element in d is a key k mapping to value v (a non-empty list).
+    Returns the sorted list of all k whose v elements sums up to a 
+    positive value.
+    """
+    # Your code here  
+
+# Examples:
+d = {5:[2,-4], 2:[1,2,3], 1:[2]}
+print(all_positive(d))   # prints the list [1, 2]
