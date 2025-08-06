3f8dcbf1487088682c580b161859b0a0a46329e8
AlinaDragu <alina_elena14@yahoo.com>
2025-08-06 11:45:31 +0300

    Intro CS

--- Intro CS/Lecture14/grades.py
+++ Intro CS/Lecture14/grades.py
@@ -1,9 +1,15 @@
+
+
 def find_grades(grades, students):
     """ grades is a dict mapping student names (str) to grades (str)
         students is a list of student names 
     Returns a list containing the grades for students (in the same order) """
     # your code here
-  
-
+    Lnew = []
+    for elem in students:
+        grade = grades[elem] 
+        Lnew.append(grade)
+    return Lnew
+ 
 d = {'Ana':'B', 'Matt':'C', 'John':'B', 'Katy':'A'}
-# print(find_grades(d, ['Matt', 'Katy'])) # returns ['C', 'A']
+print(find_grades(d, ['Matt', 'Katy'])) # returns ['C', 'A']
