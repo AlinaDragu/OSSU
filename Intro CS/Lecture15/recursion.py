9dfafeb811302000958f46d5c288e8de1303abc6
AlinaDragu <alina_elena14@yahoo.com>
2025-08-07 12:44:25 +0300

    Intro CS

---
+++ Intro CS/Lecture15/recursion.py
@@ -0,0 +1,10 @@
+# Calculate n**p recursively by writing this function
+def power_recur(n, p):
+    if ____ :
+        return ____
+    elif ____ :
+        return ____
+    else:
+        return ____
+
+# print(power_recur(2,3))  # prints 8
