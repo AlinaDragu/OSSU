6976bdc5925311d9377282dcbc48b8f5b303bb3e
AlinaDragu <alina_elena14@yahoo.com>
2025-08-07 11:31:34 +0300

    Intro CS

--- Intro CS/Lecture14/finger exercises.py
+++ Intro CS/Lecture14/finger exercises.py
@@ -6,7 +6,13 @@ def keys_with_value(aDict, target):
     Returns a sorted list of the keys in aDict with the value target.
     If aDict does not contain the value target, returns an empty list.
     """
-    # Your code here  
+    # Your code here
+    newlist = []
+    for key,value in aDict.items():
+        if value == target:
+            newlist.append(key)
+    return sorted(newlist)
+              
 
 # Examples:
 aDict = {1:2, 2:4, 5:2}
@@ -16,15 +22,19 @@ print(keys_with_value(aDict, target)) # prints the list [1,5]
 
 
 
-def all_positive(d):
-    """
-    d is a dictionary that maps int:list
-    Suppose an element in d is a key k mapping to value v (a non-empty list).
-    Returns the sorted list of all k whose v elements sums up to a 
-    positive value.
-    """
-    # Your code here  
-
-# Examples:
-d = {5:[2,-4], 2:[1,2,3], 1:[2]}
-print(all_positive(d))   # prints the list [1, 2]
+# def all_positive(d):
+#     """
+#     d is a dictionary that maps int:list
+#     Suppose an element in d is a key k mapping to value v (a non-empty list).
+#     Returns the sorted list of all k whose v elements sums up to a 
+#     positive value.
+#     """
+#     # Your code here
+#     result = []  
+#     for key,value in d.items():
+#         if sum(value) > 0:
+#             result.append(key)
+#     return sorted(result)
+# # Examples:
+# d = {5:[2,-4], 2:[1,2,3], 1:[2]}
+# print(all_positive(d))   # prints the list [1, 2]
