14cdee607c4a1a72867eb6dc7e317f193ae70b81
AlinaDragu <alina_elena14@yahoo.com>
2025-08-07 17:57:04 +0300

    Intro CS

--- Intro CS/Lecture15/finger exercises.py
+++ Intro CS/Lecture15/finger exercises.py
@@ -9,6 +9,11 @@ def recur_power(base, exp):
     case you return base * base^(exp-1).
     """
     # Your code here  
+    if exp == 0 :
+        return 1
+    else : 
+        return base * recur_power(base, exp - 1)
+        
 
 # Examples:
 print(recur_power(2,5))  # prints 32
