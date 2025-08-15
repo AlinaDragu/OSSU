bf4eb88cefacc404b611a61f75b9ba0e108b4934
AlinaDragu <alina_elena14@yahoo.com>
2025-08-15 12:11:50 +0300

    Intro CS

--- Intro CS/Lecture16/finger exercises.py
+++ Intro CS/Lecture16/finger exercises.py
@@ -6,9 +6,9 @@ def flatten(L):
     # Your code here  
     if L == []:   # base case: if the list is empty, stop recursion
         return []
-    elif isinstance(L[0],list):
-        return flatten(L[0]) + flatten(L[1:])
-    else:
+    elif isinstance(L[0],list):   # if the first element is a list
+        return flatten(L[0]) + flatten(L[1:])  # recursive step — it breaks the problem into smaller problems
+    else:   # if the first element is not a list
         return [L[0]] + flatten(L[1:])
 
 # Examples:
