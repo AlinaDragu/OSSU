0924dcef98257f4f634b0eba3bd78552e3885366
AlinaDragu <alina_elena14@yahoo.com>
2025-08-15 10:52:17 +0300

    Intro CS

---
+++ Intro CS/Lecture16/fibonacci.py
@@ -0,0 +1,9 @@
+## Fibonacci with a dictionary 
+#################
+def fib_recur(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib_recur(n-1)+fib_recur(n-2)
+
+# print(fib_recur(34))
