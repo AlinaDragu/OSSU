d24d222ad0cca31430d6065ee54031cf36f40c4a
AlinaDragu <alina_elena14@yahoo.com>
2025-08-15 12:10:41 +0300

    Intro CS

--- Intro CS/Lecture16/finger exercises.py
+++ Intro CS/Lecture16/finger exercises.py
@@ -4,6 +4,12 @@ def flatten(L):
     Returns a copy of L, which is a flattened version of L 
     """
     # Your code here  
+    if L == []:   # base case: if the list is empty, stop recursion
+        return []
+    elif isinstance(L[0],list):
+        return flatten(L[0]) + flatten(L[1:])
+    else:
+        return [L[0]] + flatten(L[1:])
 
 # Examples:
 L = [[1,4,[6],2],[[[3]],2],4,5]
