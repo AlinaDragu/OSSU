0af49860916ebd1b2a133d95d8f38d3d7da1cf2a
AlinaDragu <alina_elena14@yahoo.com>
2025-08-15 11:45:04 +0300

    Intro CS

---
+++ Intro CS/Lecture16/lists of list.py
@@ -0,0 +1,16 @@
+def in_lists_of_list(L, e):
+    """
+    L is a list whose elements are lists containing ints
+    Returns True if e is an element within the lists of L
+    and False otherwise. 
+    Hint, the in operator is useful here, i.e. e in something
+    """
+    # your code here
+
+    
+
+test = [[1,2], [3,4], [5,6,7]]
+# print(in_lists_of_list(test, 3))  # prints True
+
+test = [[1,2], [3,4], [5,6,7]]
+# print(in_lists_of_list(test, 0))  # prints False
