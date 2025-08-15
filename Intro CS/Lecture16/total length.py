74d665fef6f1ac28a8823a9030b6230e86d175ae
AlinaDragu <alina_elena14@yahoo.com>
2025-08-15 11:28:29 +0300

    Intro CS

---
+++ Intro CS/Lecture16/total length.py
@@ -0,0 +1,11 @@
+# Modify the code we wrote to return the total length 
+# of all strings inside L:   
+    
+def total_len_recur(L):
+    if len(L) == 1:
+        return ____
+    else:
+        return ____
+
+test = ["ab", "c", "defgh"]
+# print(total_len_recur(test))  # should print 8
