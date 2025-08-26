7d1ffddcb2a198f7d5296a347f6a87e1a753152e
AlinaDragu <alina_elena14@yahoo.com>
2025-08-26 18:46:18 +0300

    Intro CS

--- Intro CS/Lecture20/finger exercises.py
+++ Intro CS/Lecture20/finger exercises.py
@@ -1,2 +1,8 @@
 # In this problem, you will implement two classes according to the specification below: 
-# one Container class and one Queue class (a subclass of Container).     
+# one Container class and one Queue class (a subclass of Container).   
+  
+# Our Container class will initialize an empty list. 
+# The two methods we will have are to calculate the size of the list and to add an element. 
+# The second method will be inherited by the subclass. 
+# We now want to create a subclass so that we can add more functionality – the ability to remove elements from the list. 
+# A Queue will add elements to the list in the same way, but will behave differently when removing an element.
