1116dde85a696e8eae191c6d64b9d6c65f7ba9fd
AlinaDragu <alina_elena14@yahoo.com>
2025-08-26 18:46:51 +0300

    Intro CS

--- Intro CS/Lecture20/finger exercises.py
+++ Intro CS/Lecture20/finger exercises.py
@@ -6,3 +6,8 @@
 # The second method will be inherited by the subclass. 
 # We now want to create a subclass so that we can add more functionality – the ability to remove elements from the list. 
 # A Queue will add elements to the list in the same way, but will behave differently when removing an element.
+
+# A queue is a first-in, first-out data structure. Think of a store checkout queue. 
+# The customer who has been in the line the longest gets the next available cashier. 
+# When implementing your Queue class, you will have to think about which end of your list contains the element that has been in the list the longest.
+# This is the element you will want to remove and return.
