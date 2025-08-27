545ce0ce54b6a1633ecee9503874de789c26d675
AlinaDragu <alina_elena14@yahoo.com>
2025-08-27 11:33:16 +0300

    Intro CS

--- Intro CS/Lecture18/coordinate objetc.py
+++ Intro CS/Lecture18/coordinate objetc.py
@@ -1,3 +1,23 @@
+## EXAMPLE: simple Coordinate class
+class Coordinate(object):
+    """ A coordinate made up of an x and y value """
+    def __init__(self, x, y):
+        """ Sets the x and y values """
+        self.x = x
+        self.y = y
+    def distance(self, other):
+        """ Returns the euclidean distance between two Coordinate objects """
+        x_diff_sq = (self.x-other.x)**2
+        y_diff_sq = (self.y-other.y)**2
+        return (x_diff_sq + y_diff_sq)**0.5
+    def to_origin(self):
+        """ always sets self.x and self.y to 0,0 """
+        self.x = 0
+        self.y = 0
+    def __str__(self):
+        """ Returns a string representation of self """
+        return "<" + str(self.x) + "," + str(self.y) + ">"
+
 # #Print a coordinate object's data attributes
 c = Coordinate(3,4)
 origin = Coordinate(0,0)
