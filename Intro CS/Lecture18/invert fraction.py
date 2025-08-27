1f41eb9081c0acb96cb03bc0dbe7d179416c28ce
AlinaDragu <alina_elena14@yahoo.com>
2025-08-27 18:49:25 +0300

    Intro CS

--- Intro CS/Lecture18/invert fraction.py
+++ Intro CS/Lecture18/invert fraction.py
@@ -8,6 +8,7 @@ class SimpleFraction(object):
     def get_inverse(self):
         """ Returns a float representing 1/self """
         # your code here
+        return self.denom/self.num
         
     def invert(self):
         """ Sets self's numerator to its denominator and vice versa.
@@ -15,8 +16,9 @@ class SimpleFraction(object):
         # your code here
         
         
-# f1 = SimpleFraction(3,4)
-# print(f1.num, f1.denom)   # prints 3 4 
-# print(f1.get_inverse())   # prints 1.33333333 (note this one returns value)
+        
+f1 = SimpleFraction(3,4)
+print(f1.num, f1.denom)   # prints 3 4 
+print(f1.get_inverse())   # prints 1.33333333 (note this one returns value)
 # f1.invert()               # acts on data attributes internally, no return
 # print(f1.num, f1.denom)   # prints 4 3 
