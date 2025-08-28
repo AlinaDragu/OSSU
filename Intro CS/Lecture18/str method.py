b51ec8200226be873dc2eb2690ba9938db33167b
AlinaDragu <alina_elena14@yahoo.com>
2025-08-28 11:50:20 +0300

    Intro CS

---
+++ Intro CS/Lecture18/str method.py
@@ -0,0 +1,3 @@
+# Modify the str method to represent the Fraction as just the 
+# numerator, when the denominator is 1. Otherwise its representation 
+# is the numerator then a / then the denominator, as before
