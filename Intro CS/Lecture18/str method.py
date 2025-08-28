d1d23557aba75b070b5e97f23be09feb9b5beb03
AlinaDragu <alina_elena14@yahoo.com>
2025-08-28 11:50:45 +0300

    Intro CS

--- Intro CS/Lecture18/str method.py
+++ Intro CS/Lecture18/str method.py
@@ -1,3 +1,20 @@
 # Modify the str method to represent the Fraction as just the 
 # numerator, when the denominator is 1. Otherwise its representation 
 # is the numerator then a / then the denominator, as before
+
+class Fraction(object):
+    """ A number represented as a fraction """
+    def __init__(self, num, denom):
+        """ num and denom are integers """
+        self.num = num
+        self.denom = denom
+    def __str__(self):
+        """ Returns a string representation of self """
+        # modify this
+        return str(self.num) + "/" + str(self.denom)
+
+ 
+a = Fraction(1,4)
+b = Fraction(3,1)
+# print(a)     # prints 1/4
+# print(b)     # prints 3
