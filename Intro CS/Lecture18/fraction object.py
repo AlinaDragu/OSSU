4c453b0ce33337f3671d698c1f594ca3dd64773b
AlinaDragu <alina_elena14@yahoo.com>
2025-08-28 11:58:14 +0300

    Intro CS

--- Intro CS/Lecture18/fraction object.py
+++ Intro CS/Lecture18/fraction object.py
@@ -1,1 +1,31 @@
 # Modify the code to return a Fraction object when denominator is 1
+lass Fraction(object):
+    def __init__(self, num, denom):
+        """ num and denom are integers """
+        self.num = num
+        self.denom = denom
+    def reduce(self):
+        def gcd(n, d):
+            while d != 0:
+                (d, n) = (n%d, d)
+            return n
+        if self.denom == 0:
+            return None
+        elif self.denom == 1:
+            # modify this
+            return self.num
+        else:
+            greatest_common_divisor = gcd(self.num, self.denom)
+            top = int(self.num/greatest_common_divisor)
+            bottom = int(self.denom/greatest_common_divisor)
+            return Fraction(top, bottom)
+    def __str__(self):
+        """ Returns a string representation of self """
+        # Note this is not the version with the numerator 
+        # only when the denomiator is 1
+        return str(self.num) + "/" + str(self.denom)
+    
+f1 = Fraction(5,1)
+f1r = f1.reduce()
+# print(f1r)          # prints 5/1 not 5
+# print(type(f1r))    # prints <class '__main__.Fraction'>
