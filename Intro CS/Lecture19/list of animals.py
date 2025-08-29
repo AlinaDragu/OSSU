77ee80e86b88d1ace2c397914c97e11fc1a20cb8
AlinaDragu <alina_elena14@yahoo.com>
2025-08-29 17:57:30 +0300

    Intro CS

--- Intro CS/Lecture19/list of animals.py
+++ Intro CS/Lecture19/list of animals.py
@@ -1,1 +1,17 @@
 # Write a function that meets this spec.
+
+def make_animals(L1, L2):
+    """ L1 is a list if ints and L2 is a list of str
+        L1 and L2 have the same length
+    Creates a list of Animals the same length as L1 and L2.
+    An animal object at index i has the age and name
+    corresponding to the same index in L1 and L2, respectively. """
+    # your code here
+
+
+L1 = [2,5,1]
+L2 = ["blobfish", "crazyant", "parafox"]
+# animals = make_animals(L1, L2)
+# print(animals)     # note this prints a list of animal objects
+# for i in animals:  # this prints the indivdual animals
+#     print(i)
