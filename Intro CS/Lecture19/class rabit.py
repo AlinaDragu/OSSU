69ecc38a3ba8b585eb3911aa789ac577a80858f3
AlinaDragu <alina_elena14@yahoo.com>
2025-09-01 11:42:57 +0300

    Intro CS

--- Intro CS/Lecture19/class rabit.py
+++ Intro CS/Lecture19/class rabit.py
@@ -1,4 +1,21 @@
-lass Rabbit(Animal):
+import random
+
+class Animal(object):
+    def __init__(self, age):
+        self.age = age
+        self.name = None
+    def __str__(self):
+        return "animal:"+str(self.name)+":"+str(self.age)
+    def get_age(self):
+        return self.age
+    def get_name(self):
+        return self.name
+    def set_age(self, newage):
+        self.age = newage
+    def set_name(self, newname=""):
+        self.name = newname
+
+class Rabbit(Animal):
     """ A subclass of Animal """
     def speak(self):
         """ prints the string meep to the console """
@@ -7,8 +24,8 @@ lass Rabbit(Animal):
         """ Repr as "rabbit", a colon, its name, a colon, its age """
         return "rabbit:"+str(self.get_name())+":"+str(self.get_age())
    
-# c = Rabbit(5)
-# print(c)
-# c.speak()
-# c.set_name('fluffy')
-# print(c)
+c = Rabbit(5)
+print(c)
+c.speak()
+c.set_name('fluffy')
+print(c)
