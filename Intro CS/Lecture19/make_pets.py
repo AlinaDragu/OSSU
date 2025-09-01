d11ecd92d94086b69aefcdfeb14ba417870919de
AlinaDragu <alina_elena14@yahoo.com>
2025-09-01 11:47:04 +0300

    Intro CS

--- Intro CS/Lecture19/make_pets.py
+++ Intro CS/Lecture19/make_pets.py
@@ -1,9 +1,56 @@
+import random
+
+class Animal(object):
+    def __init__(self, age):
+        self.age = age
+        self.name = None
+    def __str__(self):
+        return "animal:"+str(self.name)+":"+str(self.age)
+    def get_age(self):
+        return self.age
+    def get_name(self):
+        return self.name
+    def set_age(self, newage):
+        self.age = newage
+    def set_name(self, newname=""):
+        self.name = newname
+        
+class Cat(Animal):
+    def speak(self):
+        print("meow")
+    def __str__(self):
+        return "cat:"+str(self.name)+":"+str(self.age)
+
+class Person(Animal):
+    def __init__(self, name, age):
+        Animal.__init__(self, age)
+        self.set_name(name)
+        self.friends = []
+    def get_friends(self):
+        return self.friends.copy()
+    def speak(self):
+        print("hello")
+    def add_friend(self, fname):
+        if fname not in self.friends:
+            self.friends.append(fname)
+    def age_diff(self, other):
+        diff = self.age - other.age
+        print(abs(diff), "year difference")
+    def __str__(self):
+        return "person:"+str(self.name)+":"+str(self.age)
+
+
 # Write the function according to this spec
 def make_pets(d):
     """ d is a dict mapping a Person obj to a Cat obj
     Prints, on each line, the name of a person, 
     a colon, and the name of that person's cat """
     # your code here
+    for k,v in d.items():
+        # k is a Person
+        # v is Cat
+        print(k.get_name()+ ':' + v.get_name())
+        
 
 
 p1 = Person("ana", 86)
