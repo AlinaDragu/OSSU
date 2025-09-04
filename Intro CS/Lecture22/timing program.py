07e29c42a084ec6de83e0b4212fbaa77193867b2
AlinaDragu <alina_elena14@yahoo.com>
2025-09-04 17:35:37 +0300

    Intro CS

--- Intro CS/Lecture22/timing program.py
+++ Intro CS/Lecture22/timing program.py
@@ -1,3 +1,18 @@
 ## -------------------------------------------------- ##
 ## EXAMPLE: timing a program
 ## -------------------------------------------------- ##
+
+## define two functions
+def convert_to_km(m):
+    return m * 1.609
+
+def compound(invest, interest, n_months):
+    total=0
+    for i in range(n_months):
+       total = total * interest + invest
+    return total
+
+##creates a list [1, 10, 100, ...] to test different input sizes
+L_N = [1]
+for i in range(8):
+    L_N.append(L_N[-1]*10)
