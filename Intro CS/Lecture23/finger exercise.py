e82d68456e63a4ef11f5ac3f4c85a365ab176a25
AlinaDragu <alina_elena14@yahoo.com>
2025-09-05 20:05:58 +0300

    Intro CS

--- Intro CS/Lecture23/finger exercise.py
+++ Intro CS/Lecture23/finger exercise.py
@@ -2,3 +2,11 @@
 # order of growth (upper and lower bound) for the following function. 
 # Assume n = a.
 
+def running_product(a):
+    """ a is an int """
+    product = 1
+    for i in range(5,a+5):
+        product *= i
+        if product == a:
+            return True
+    return False
